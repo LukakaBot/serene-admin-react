b52d9a60ad2a7836411d24ea2b492ee6d376bdb4
Lukaka <monishine_code@163.com>
2025-02-14 20:27:55 +0800

    feat(deps-dev): unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+export default defineConfig(() => {
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      AutoImport({
+        imports: [
+          'react',
+          'react-router',
+        ],
+        dts: 'src/types/auto-import.d.ts',
+        include: [
+          /\.[tj]sx?$/,
+          /\.md$/
+        ]
+      })
+    ],
+  }
 })
