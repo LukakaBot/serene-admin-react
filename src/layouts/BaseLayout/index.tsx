a932c2702fd8e7a0cea487180b6f65a9adc1665d
Lukaka <monishine_code@163.com>
2025-02-17 15:27:02 +0800

    feat(layout): 调整layout引用组件目录结构

--- src/layouts/BaseLayout/index.tsx
+++ src/layouts/BaseLayout/index.tsx
@@ -1,10 +1,10 @@
 import type { CSSProperties, FC } from 'react';
 import { Layout } from 'antd';
 import type { BaseLayoutProps } from './props';
-import BaseHeader from './components/BaseHeader';
-import BaseSider from './components/BaseSider';
-import BaseContent from './components/BaseContent';
-import BaseFooter from './components/BaseFooter';
+import BaseHeader from '@/components/BaseHeader';
+import BaseSider from '@/components/BaseSider';
+import BaseContent from '@/components/BaseContent';
+import BaseFooter from '@/components/BaseFooter';
 
 const layoutStyle: CSSProperties = {
 	overflow: 'hidden',
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 
@@ -21,5 +22,10 @@ export default defineConfig(() => {
         ]
       })
     ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src")
+      }
+    }
   }
 })
