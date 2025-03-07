49d411996c9d57c64b4705dceab0f1c6fb9f535b
Lukaka <monishine_code@163.com>
2025-03-07 17:22:43 +0800

    feat(pages): 登录页添加登录成功后的页面跳转

--- src/pages/login/components/LoginBox.tsx
+++ src/pages/login/components/LoginBox.tsx
@@ -1,55 +1,58 @@
-import type { FormRule } from 'antd';
-import { Card, Form, Input, Button } from 'antd';
-import type { AccountUserTokenParams } from '@/api/system/user/types';
-import useLoading from '@/hooks/loading/useLoading';
-import userStore from '@/store/user';
+import type { FormRule } from "antd";
+import { Card, Form, Input, Button } from "antd";
+import type { AccountUserTokenParams } from "@/api/system/user/types";
+import useLoading from "@/hooks/loading/useLoading";
+import userStore from "@/store/user";
+import { useRouter } from "@/hooks/index";
 
 const rules: Record<keyof AccountUserTokenParams, FormRule[]> = {
-	username: [{ required: true, message: '请输入用户名' }],
-	password: [{ required: true, message: '请输入密码' }],
+  username: [{ required: true, message: "请输入用户名" }],
+  password: [{ required: true, message: "请输入密码" }],
 };
 
 const LoginBox: React.FC = () => {
-	const [loading, setLoading] = useLoading();
-	const { getAccountUserToken } = userStore();
+  const [loading, setLoading] = useLoading();
+  const { getAccountUserToken } = userStore();
+  const router = useRouter();
 
-	const [formData] = useState<AccountUserTokenParams>({
-		username: 'admin',
-		password: '123456',
-	});
+  const [formData] = useState<AccountUserTokenParams>({
+    username: "admin",
+    password: "123456",
+  });
 
-	async function handleFinish(values: AccountUserTokenParams) {
-		try {
-			setLoading(true);
-			await getAccountUserToken(values);
-		} finally {
-			setLoading(false);
-		}
-	}
+  async function handleFinish(values: AccountUserTokenParams) {
+    try {
+      setLoading(true);
+      await getAccountUserToken(values);
+      router.replace("/dashboard/home");
+    } finally {
+      setLoading(false);
+    }
+  }
 
-	return (
-		<div className='flex justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
-			<Card style={{ width: 300 }}>
-				<Form
-					labelCol={{ span: 6 }}
-					initialValues={formData}
-					onFinish={handleFinish}
-				>
-					<Form.Item label='用户名' name='username' rules={rules.username}>
-						<Input placeholder='请输入用户名' />
-					</Form.Item>
-					<Form.Item label='密码' name='password' rules={rules.password}>
-						<Input.Password placeholder='请输入密码' />
-					</Form.Item>
-					<Form.Item>
-						<Button block type='primary' htmlType='submit' loading={loading}>
-							登录
-						</Button>
-					</Form.Item>
-				</Form>
-			</Card>
-		</div>
-	);
+  return (
+    <div className="flex justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Card style={{ width: 300 }}>
+        <Form
+          labelCol={{ span: 6 }}
+          initialValues={formData}
+          onFinish={handleFinish}
+        >
+          <Form.Item label="用户名" name="username" rules={rules.username}>
+            <Input placeholder="请输入用户名" />
+          </Form.Item>
+          <Form.Item label="密码" name="password" rules={rules.password}>
+            <Input.Password placeholder="请输入密码" />
+          </Form.Item>
+          <Form.Item>
+            <Button block type="primary" htmlType="submit" loading={loading}>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 };
 
 export default LoginBox;
