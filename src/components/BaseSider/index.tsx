29e58b4dcc565066529d0bbd82b88ca4ceadd79c
Lukaka <monishine_code@163.com>
2025-03-11 14:45:40 +0800

    feat(layout): 调整BaseSider组件

--- src/components/BaseSider/index.tsx
+++ src/components/BaseSider/index.tsx
@@ -1,19 +1,41 @@
-import type { FC, CSSProperties } from 'react';
-import { Layout } from 'antd';
+import type { CSSProperties } from "react";
+import { Layout, Menu } from "antd";
+import type { BaseSiderProps } from "./types";
+import useRouteStore from "@/store/route";
 
 const { Sider } = Layout;
 
-const siderStyle: CSSProperties = {
-	textAlign: 'center',
-	lineHeight: '120px',
-	color: '#fff',
-	backgroundColor: '#1677ff',
+const defaultSiderStyle: CSSProperties = {
+  textAlign: "center",
+  lineHeight: "120px",
+  color: "#fff",
 };
 
-const BaseSider: FC = () => (
-	<Sider width='25%' style={siderStyle}>
-		Sider
-	</Sider>
-);
+function BaseSider(props: BaseSiderProps) {
+  const { siderStyle } = props;
+  const [collapsed, setCollapsed] = useState(false);
+  const { routes } = useRouteStore();
+
+  const mergedSiderStyle = {
+    ...defaultSiderStyle,
+    ...siderStyle,
+  };
+
+  return (
+    <Sider
+      collapsible
+      collapsed={collapsed}
+      onCollapse={(value) => setCollapsed(value)}
+      style={mergedSiderStyle}
+    >
+      <Menu
+        items={routes}
+        theme="dark"
+        defaultSelectedKeys={["1"]}
+        mode="inline"
+      />
+    </Sider>
+  );
+}
 
 export default BaseSider;
---
+++ src/components/BaseSider/types.d.ts
@@ -0,0 +1,5 @@
+import type { CSSProperties } from "react";
+
+interface BaseSiderProps {
+  siderStyle: CSSProperties;
+}
