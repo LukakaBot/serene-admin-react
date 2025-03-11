220a3cdff019728ebfcb3656ea389c63c8132dce
Lukaka <monishine_code@163.com>
2025-03-11 15:52:26 +0800

    chore(comp): 优化BaseContent组件

--- src/components/BaseContent/index.tsx
+++ src/components/BaseContent/index.tsx
@@ -1,20 +1,21 @@
-import type { FC, CSSProperties } from 'react';
-import { Layout } from 'antd';
-import type { BaseContentProps } from './props';
+import type { CSSProperties } from "react";
+import { Layout } from "antd";
+import type { BaseContentProps } from "./props";
 
 const { Content } = Layout;
 
 const contentStyle: CSSProperties = {
-	display: 'flex',
-	flex: '1',
-	flexDirection: 'column',
-	textAlign: 'center',
-	color: '#fff',
-	backgroundColor: '#0958d9',
+  display: "flex",
+  flex: "1",
+  flexDirection: "column",
+  textAlign: "center",
+  color: "#fff",
+  backgroundColor: "#0958d9",
 };
 
-const BaseContent: FC<BaseContentProps> = (props) => {
-	return <Content style={contentStyle}>{props.children}</Content>;
-};
+function BaseContent(props: BaseContentProps) {
+  const { children } = props;
+  return <Content style={contentStyle}>{children}</Content>;
+}
 
 export default BaseContent;
