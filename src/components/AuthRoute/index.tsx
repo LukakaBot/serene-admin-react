62c158864343d8eae87383d7ba2cd9f8ada568b1
Lukaka <monishine_code@163.com>
2025-03-11 10:26:16 +0800

    chore(comp): 优化AuthRoute组件

--- src/components/AuthRoute/index.tsx
+++ src/components/AuthRoute/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import type { RouteObject } from "react-router";
 import { useRoutes } from "react-router";
 import type { BaseRouteObject } from "@/router/interface";
@@ -7,56 +6,57 @@ import RouteGuard from "@/components/RouteGuard";
 
 interface AuthRouteProps {
   routes: BaseRouteObject[];
-  children?: React.ReactNode;
 }
 
 function transformRoute(route: BaseRouteObject): RouteObject {
-  const {
-    index,
-    element,
-    children,
-    type,
-    requiresAuth = true,
-    ...rest
-  } = route;
-  const isIndex = index ?? false;
+  try {
+    const {
+      index,
+      element,
+      children,
+      type,
+      requiresAuth = true,
+      ...rest
+    } = route;
+    const isIndex = index ?? false;
 
-  const PageElement = element
-    ? type === "layout"
-      ? lazyImportLayout(element)
-      : lazyImportRoute(element)
-    : undefined;
+    const PageElement =
+      element &&
+      (type === "layout"
+        ? lazyImportLayout(element)
+        : lazyImportRoute(element));
 
-  const renderedElement = PageElement && <PageElement />;
-  const guardedElement =
-    renderedElement && requiresAuth ? (
-      <RouteGuard>{renderedElement}</RouteGuard>
-    ) : (
-      renderedElement
-    );
+    const renderedElement = PageElement && <PageElement />;
+    const guardedElement =
+      renderedElement && requiresAuth ? (
+        <RouteGuard>{renderedElement}</RouteGuard>
+      ) : (
+        renderedElement
+      );
 
-  if (isIndex) {
+    if (isIndex) {
+      return {
+        ...rest,
+        index: true,
+        element: guardedElement,
+      };
+    }
+
     return {
       ...rest,
-      index: true,
+      index: false,
       element: guardedElement,
+      children: children?.length ? children.map(transformRoute) : undefined,
     };
+  } catch (error) {
+    console.error(error);
+    return { ...route, element: null } as RouteObject;
   }
-
-  return {
-    ...rest,
-    index: false,
-    element: guardedElement,
-    children: children?.length ? children.map(transformRoute) : undefined,
-  };
 }
 
-function AuthRoute(props: AuthRouteProps) {
-  const { routes } = props;
+function AuthRoute({ routes }: AuthRouteProps) {
   const processRoutes = useMemo(() => routes.map(transformRoute), [routes]);
-  const Routes = useRoutes(processRoutes);
-
-  return Routes;
+  return useRoutes(processRoutes);
 }
 
 export default AuthRoute;
