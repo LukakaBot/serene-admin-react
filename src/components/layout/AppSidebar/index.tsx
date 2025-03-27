3ca0f6fd6cedd7922ba9f18edac48566797917a2
鲨鱼辣椒 <monishine_code@163.com>
2025-03-27 11:08:45 +0800

    refactor: 重构AppSidebar

--- src/components/layout/AppSidebar/index.tsx
+++ src/components/layout/AppSidebar/index.tsx
@@ -1,47 +1,24 @@
-import type { CSSProperties } from "react";
+import type { MenuProps } from "antd";
 import { Layout, Menu } from "antd";
-import type { AppSidebarProps } from "./types";
-import useRouteStore from "@/store/route";
 
 const { Sider } = Layout;
 
-const defaultSidebarStyle: CSSProperties = {
-  textAlign: "center",
-  lineHeight: "120px",
-  color: "#fff",
-};
-
-function AppSidebar(props: AppSidebarProps) {
-  const { sidebarStyle } = props;
+function AppSidebar() {
   const [collapsed, setCollapsed] = useState(false);
-  const { routes } = useRouteStore();
-
-  const mergedSidebarStyle = {
-    ...defaultSidebarStyle,
-    ...sidebarStyle,
-  };
-
-  const menus = useMemo(() => {
-    return routes;
-  }, [routes]);
-
-  useEffect(() => {
-    console.log(menus);
-  }, [menus]);
+  const items: MenuProps["items"] = [];
 
   return (
     <Sider
       collapsible
       collapsed={collapsed}
       onCollapse={(value) => setCollapsed(value)}
-      style={mergedSidebarStyle}
     >
-      <div className="m-4 h-8 rounded-md bg-[rgba(255,255,255,0.2)]" />
+      <div className="demo-logo-vertical" />
       <Menu
         theme="dark"
         defaultSelectedKeys={["1"]}
         mode="inline"
-        items={menus}
+        items={items}
       />
     </Sider>
   );
--- src/components/layout/AppSidebar/types.d.ts
+++
@@ -1,6 +0,0 @@
-import type { CSSProperties } from "react";
-
-interface AppSidebarProps {
-  sidebarStyle?: CSSProperties;
-  children?: React.ReactNode;
-}
